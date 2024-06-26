aed15061c8b2c0f895000d0fe80f5068980ea670
Aste <aste@tuta.io>
2024-06-26 17:52:51 +0200

    Add generateId and app.post functionality for new notes, as well as new test cases to the .rest file

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+app.use(express.json());
+
 let notes = [
   {
     id: 1,
@@ -35,6 +42,26 @@ app.delete("/api/notes/:id", (req, res) => {
   notes = notes.filter((note) => note.id !== id);
 
   res.status(204).end();
+});
+
+app.post("/api/notes", (req, res) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
 });
 
 app.use((req, res) => {
